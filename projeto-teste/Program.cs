cbd34d604f0c91e6c88f74324ad6e5885c1fe825
Augusto Aleff <augustoaleff@gmail.com>
2018-01-12 19:34:41 -0200

    AcrescentaDA bom dia

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -7,6 +7,10 @@ using System.Threading.Tasks;
 namespace projeto_teste {
     class Program {
         static void Main(string[] args) {
+
+            Console.WriteLine("Olá Mundo!");
+            Console.WriteLine("Bom Dia");
+            Console.ReadLine();
         }
     }
 }
